a0774bd8f57749f880908a66d897a630004b23d7
신정아 <jeonga@khu.ac.kr>
2022-02-20 14:04:51 +0900

    보드 서비스 코드 - 자동 컴포넌트 방식

--- src/main/java/hello/boardspring/service/BoardService.java
+++ src/main/java/hello/boardspring/service/BoardService.java
@@ -2,10 +2,14 @@ package hello.boardspring.service;
 
 import hello.boardspring.domain.Board;
 import hello.boardspring.repository.RealBoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+//이 때, boardSerivce 클래스에 @Service라는 어노테이션이ㅣ 있어야 함.
+//자동 컴포넌트 방식// @Service//이게 있어야 스프링이 얘를 찾아서 연결시켜줄 수 있음. 없으면 못 찾음
 public class BoardService {
     
     //일단 회원 서비스를 만들려면 현재 저장소에 어떤 board가 있는지 현황?을 알아야 하니까
@@ -20,6 +24,12 @@ public class BoardService {
     
     private final RealBoardRepository realBoardRepository;
     
+    
+    //자동 컴포넌트 방식
+    //Autowired가 있으면 그 때, 스프링이 파라미터에 있는 realRepository를 찾아서 연결시켜줌
+    //마찬가지로 리포지토리 클래스에도 @Repository라는 어노테이션이 있어야 스프링이 리포지토리로 찾아낼 수 있음
+    //해당 리포지토리 클래스에 @Repository 이게 있어야 스프링이 얘를 찾아서 연결시켜줄 수 있음. 없으면 못 찾음
+    //@Autowired
     public BoardService(RealBoardRepository realBoardRepository){ //외부?에서 자기가 쓸 리포지토리를 서비스의 생성자의 파라미터로 주입하면
         //보드 서비스의 리포지토리는 그 외부(ex. 테스트코드)가 사용하는 리포지토리와 동일한 리포지토리를 갖게 된다(쓴다)
         this.realBoardRepository=realBoardRepository;
