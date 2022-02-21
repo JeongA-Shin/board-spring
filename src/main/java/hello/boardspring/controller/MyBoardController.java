a5a8eaf0c9a6c20ba1d98b9f06ac5d0df106276c
신정아 <jeonga@khu.ac.kr>
2022-02-22 00:04:49 +0900

    컨트롤러 완성 - 설명 및 HTML 작업 필요

--- src/main/java/hello/boardspring/controller/MyBoardController.java
+++ src/main/java/hello/boardspring/controller/MyBoardController.java
@@ -1,21 +1,25 @@
 package hello.boardspring.controller;
 
 
+import hello.boardspring.domain.MyBoard;
+import hello.boardspring.repository.MyBoardRepository;
 import hello.boardspring.service.BoardService;
 import hello.boardspring.service.MyBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/myboard")
 public class MyBoardController {
     
-    private MyBoardService myBoardService;
+    private final  MyBoardService myBoardService;
+    private MyBoardRepository myBoardRepository;
     
     @Autowired
     public MyBoardController(MyBoardService myBoardService){
@@ -29,7 +33,55 @@ public class MyBoardController {
     public String list(@PageableDefault Pageable pageable, Model model){
         model.addAttribute("boardList",myBoardService.findBoardList(pageable));
         //컨트롤러에서 뷰에 어떤 정보등을 넘겨줘야 하면 반드시 model에 담아서 줘야 한다
-        return "myboard/list";
+        return "/myboard/list";
     }
+    
+    /*
+     * 게시글 상세 및 등록 폼 호출
+     */
+    @GetMapping({"", "/"})
+    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) {
+        model.addAttribute("board", myBoardService.findBoardByIdx(idx));
+        return "/board/form";
+    }
+    
+    /*
+     * 게시글 생성
+     */
+    @PostMapping
+    public ResponseEntity<?> postBoard(@RequestBody MyBoard myBoard) {
+//        board.setCreatedDate(LocalDateTime.now());
+//        board.setUpdatedDate(LocalDateTime.now());
+         myBoardRepository.save(myBoard);
+        
+        return new ResponseEntity<>("{}", HttpStatus.CREATED);
+    }
+    
+    /*
+     * 게시글 수정
+     */
+    @PutMapping("/{idx}")
+    public ResponseEntity<?> putBoard(@PathVariable("idx") Long idx, @RequestBody MyBoard myBoard) {
+        MyBoard updateBoard = myBoardRepository.getOne(idx); //해당 객체를 가져오고
+        updateBoard.setTitle(myBoard.getTitle());
+        updateBoard.setContent(myBoard.getContent());
+        //updateBoard.setUpdatedDate(LocalDateTime.now());
+        myBoardRepository.save(updateBoard);
+        
+        return new ResponseEntity<>("{}", HttpStatus.OK);
+    }
+    
+    
+    /*
+            * 게시글 삭제
+     */
+    @DeleteMapping("/{idx}")
+    public ResponseEntity<?> deleteBoard(@PathVariable("idx") Long idx) {
+        myBoardRepository.deleteById(idx);
+        return new ResponseEntity<>("{}", HttpStatus.OK);
+    }
+    
+    
+    
 }
 
