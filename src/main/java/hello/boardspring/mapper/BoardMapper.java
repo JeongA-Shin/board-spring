399d81f807d5d14594c292eb3ea889fb240419c2
신정아 <shinjeonga@twentyoz.kr>
2022-03-18 15:44:07 +0900

    modify - 매퍼

--- src/main/java/hello/boardspring/mapper/BoardMapper.java
+++ src/main/java/hello/boardspring/mapper/BoardMapper.java
@@ -2,12 +2,16 @@ package hello.boardspring.mapper;
 
 import hello.boardspring.domain.Board;
 import org.mapstruct.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-@Mapper
+
+
+@Mapper(componentModel = "spring") //해당 속성 반드시!!! 해줘야 함!!- spring과 mapstruct 같이 쓸 땐 무조건
 public abstract class BoardMapper {
 
-  //근데 굳이 modify함수를 매퍼로 처리할 필요가 있나? 그냥 이거 자체가 매핑/덮어씌우는 거 아닌가?
-  public Board modify(Board in, Board out) { //in이 수정하고 싶은 내용이 담긴 수정본, out은 그냥 단순히 덮어씌워질 기존 객체
+  //근데 굳이 modify함수를 매퍼로 처리할 필요가 있나? 그냥 이거 자체가 매핑/덮어씌우는 거 아닌가? - 일단 구조는 이렇게 해두자
+  public Board modify(Board in, Board out) { //in이 수정하고 싶은 내용이 담긴 수정본, out은 그냥 단순히 덮어씌워질 "기존 "객체
     if(in == null){
       return null;
     }
