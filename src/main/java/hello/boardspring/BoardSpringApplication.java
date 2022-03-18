c7862bad3cff8abae48486938b2013102b897615
신정아 <shinjeonga@twentyoz.kr>
2022-03-18 15:21:03 +0900

    정리

--- src/main/java/hello/boardspring/BoardSpringApplication.java
+++ src/main/java/hello/boardspring/BoardSpringApplication.java
@@ -1,12 +1,9 @@
 package hello.boardspring;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-import java.time.LocalDateTime;
-import java.util.stream.IntStream;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
