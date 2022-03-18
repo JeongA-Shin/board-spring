bdcf413c68208509aa848ee6c548b0fc45750e38
신정아 <shinjeonga@twentyoz.kr>
2022-03-18 16:26:33 +0900

    entity dto 매핑

--- src/main/generated/hello/boardspring/mapper/BoardMapperImpl.java
+++ src/main/generated/hello/boardspring/mapper/BoardMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-03-18T14:50:03+0900",
+    date = "2022-03-18T16:19:49+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_322 (Temurin)"
 )
 @Component
