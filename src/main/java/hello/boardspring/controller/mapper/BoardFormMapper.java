5594a6314e9787524483ef97ef5e2101c2d8b0bc
신정아 <shinjeonga@twentyoz.kr>
2022-03-18 16:27:34 +0900

    dto->entity 매핑 추가

--- src/main/java/hello/boardspring/controller/mapper/BoardFormMapper.java
+++ src/main/java/hello/boardspring/controller/mapper/BoardFormMapper.java
@@ -9,13 +9,14 @@ import org.mapstruct.Mapper;
 
 @Mapper(
     implementationName = "BoardFormMapperImpl",
-    builder=@Builder(disableBuilder = true)
+    builder=@Builder(disableBuilder = true),
+    componentModel = "spring"
 )
 public abstract class BoardFormMapper { //Board는 엔티티, BoardForm은 DTO
 
   // dto가 파라미터로 들어오면 엔티티로 매핑함
-//  public abstract Board toBoard(BoardForm.Input.Add in);
-//  public abstract Board toBoard(BoardForm.Input.Modify in);
+  public abstract Board toBoard(BoardForm.Input.Add in);
+  public abstract Board toBoard(BoardForm.Input.Modify in);
 
   //엔티티를  dto로
   public abstract BoardForm.OutPut.Get toGet(Board in); //객체를 dto(form)객체(형식으)로 바꿈
