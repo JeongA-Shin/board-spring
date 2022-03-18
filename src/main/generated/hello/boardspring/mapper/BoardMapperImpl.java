6906c8b33966717c53edbae75a8d720cdc311793
신정아 <shinjeonga@twentyoz.kr>
2022-03-18 15:43:34 +0900

    modify 매퍼 자동생성

---
+++ src/main/generated/hello/boardspring/mapper/BoardMapperImpl.java
@@ -0,0 +1,13 @@
+package hello.boardspring.mapper;
+
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-03-18T14:50:03+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_322 (Temurin)"
+)
+@Component
+public class BoardMapperImpl extends BoardMapper {
+}
