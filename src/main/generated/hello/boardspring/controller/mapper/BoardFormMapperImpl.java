291b910c0183ddb243d5c20c97dc92fb4f16e8f4
신정아 <shinjeonga@twentyoz.kr>
2022-03-21 11:35:56 +0900

    CRUD API 완성 - SWAGGER 연동까지 확인

--- src/main/generated/hello/boardspring/controller/mapper/BoardFormMapperImpl.java
+++ src/main/generated/hello/boardspring/controller/mapper/BoardFormMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-03-18T16:49:29+0900",
+    date = "2022-03-21T10:36:44+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_322 (Temurin)"
 )
 @Component
